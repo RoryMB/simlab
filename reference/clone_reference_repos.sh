a5e00e73f4ddcb8befb53c54c307cb151084402b
Rory Butler <34782324+RoryMB@users.noreply.github.com>
2025-08-06 21:07:10 -0500

    Added reference materials for colorpicker experiment

---
+++ reference/clone_reference_repos.sh
@@ -0,0 +1,34 @@
+git clone https://github.com/AD-SDL/MADSci.git MADSci_Core
+
+git clone https://github.com/AD-SDL/MADSci_Examples.git MADSci_Examples
+
+mkdir MADSci_Experiments
+cd MADSci_Experiments
+git clone https://github.com/AD-SDL/colorpicker_experiment.git MADSci_Experiments/colorpicker_experiment
+cd ..
+
+mkdir MADSci_Modules
+cd MADSci_Modules
+git clone https://github.com/AD-SDL/a4s_sealer_module.git
+git clone https://github.com/AD-SDL/barty_module.git
+git clone https://github.com/AD-SDL/big_kahuna_module.git
+git clone https://github.com/AD-SDL/brooks_xpeel_module.git
+git clone https://github.com/AD-SDL/camera_module.git
+git clone https://github.com/AD-SDL/hidex_module.git
+git clone https://github.com/AD-SDL/hudson_platecrane_module.git
+git clone https://github.com/AD-SDL/hudson_solo_module.git
+git clone https://github.com/AD-SDL/inheco_incubator_module.git
+git clone https://github.com/AD-SDL/labjack_module.git
+git clone https://github.com/AD-SDL/liconic_module.git
+git clone https://github.com/AD-SDL/metrohm_m101_module.git
+git clone https://github.com/AD-SDL/object_detection_module.git
+git clone https://github.com/AD-SDL/ot2_module.git
+git clone https://github.com/AD-SDL/pal_module.git
+git clone https://github.com/AD-SDL/pe_icp_module.git
+git clone https://github.com/AD-SDL/pf400_module.git
+git clone https://github.com/AD-SDL/sciclops_module.git
+git clone https://github.com/AD-SDL/transfer_meta_module.git
+git clone https://github.com/AD-SDL/ur_module.git
+git clone https://github.com/AD-SDL/vention_rail_module.git
+git clone https://github.com/AD-SDL/zigbee_module.git
+cd ..
